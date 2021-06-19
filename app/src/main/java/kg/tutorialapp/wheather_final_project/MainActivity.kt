4ff7bf0b93a4f4654f8a0ad93a80ec566b803940
Jyldyz Sultanbekova <sultanbekova.jyldyz@gmail.com>
2021-06-19 18:08:36 +0600

    put, patch and delete, lesson 49

--- app/src/main/java/kg/tutorialapp/wheather_final_project/MainActivity.kt
+++ app/src/main/java/kg/tutorialapp/wheather_final_project/MainActivity.kt
@@ -16,6 +16,29 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
+    private val okHttp by lazy {
+        val interceptor = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
+
+        OkHttpClient.Builder().addInterceptor(interceptor).build()
+    }
+
+    private val retrofit by lazy {
+        Retrofit.Builder()
+                //.baseUrl("https://api.openweathermap.org/data/2.5/")
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create()).client(okHttp)
+                .build()
+    }
+
+    private val weatherApi by lazy {
+        retrofit.create(WeatherApi::class.java)
+    }
+
+    private val postsApi by lazy {
+        retrofit.create(PostsApi::class.java)
+    }
+
+
     lateinit var textView: TextView
     lateinit var textView2: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,7 +53,52 @@ class MainActivity : AppCompatActivity() {
         //fetchPostById()
         //createPost()
         //createPostUsingFields()
-        createPostUsingFieldsMap()
+        //createPostUsingFieldsMap()
+        //updatePost()
+        deletePost()
+    }
+
+    private fun deletePost() {
+        val call = postsApi.deletePost("42")
+
+        call.enqueue(object: Callback<Unit>{
+            override fun onResponse(call: Call<Unit>, response: Response<Unit>) {
+                textView.text = response.code().toString()
+            }
+
+            override fun onFailure(call: Call<Unit>, t: Throwable) {
+
+            }
+        })
+    }
+
+    private fun updatePost() {
+
+        val newPost = Post(userId = 20, body = "this is body" )
+
+        val call = postsApi.patchPost(id = "42", post = newPost)
+
+        call.enqueue(object : Callback<Post> {
+            override fun onResponse(call: Call<Post>, response: Response<Post>) {
+
+                val resultPost = response.body()
+
+                resultPost?.let {
+                    val resultText = "ID:" + it.id + "\n" +
+                            "userID:" + it.userId + "\n" +
+                            "TITLE:" + it.title + "\n" +
+                            "BODY:" + it.body + "\n"
+
+                    textView.text = resultText
+                }
+
+            }
+
+            override fun onFailure(call: Call<Post>, t: Throwable) {
+
+
+            }
+        })
     }
 
     private fun createPostUsingFieldsMap() {
@@ -42,7 +110,7 @@ class MainActivity : AppCompatActivity() {
 
         val call = postsApi.createPostUsingFieldsMap(map)
 
-        call.enqueue(object: Callback<Post>{
+        call.enqueue(object : Callback<Post> {
             override fun onResponse(call: Call<Post>, response: Response<Post>) {
 
                 val resultPost = response.body()
@@ -69,7 +137,7 @@ class MainActivity : AppCompatActivity() {
 
         val call = postsApi.createPostUsingFields(userId = "99", title = "HI!!!", body = "Osh")
 
-        call.enqueue(object: Callback<Post>{
+        call.enqueue(object : Callback<Post> {
             override fun onResponse(call: Call<Post>, response: Response<Post>) {
 
                 val resultPost = response.body()
@@ -97,7 +165,7 @@ class MainActivity : AppCompatActivity() {
 
         val call = postsApi.createPost(post)
 
-        call.enqueue(object: Callback<Post>{
+        call.enqueue(object : Callback<Post> {
             override fun onResponse(call: Call<Post>, response: Response<Post>) {
 
                 val resultPost = response.body()
@@ -123,7 +191,7 @@ class MainActivity : AppCompatActivity() {
     private fun fetchPostById() {
         val call = postsApi.fetchPostById(10)
 
-        call.enqueue(object: Callback<Post>{
+        call.enqueue(object : Callback<Post> {
             override fun onResponse(call: Call<Post>, response: Response<Post>) {
 
                 val post = response.body()
@@ -147,7 +215,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun fetchWeatherUsingQuery() {
-        val call = weatherApi.fetchWeatherUsingQuery(lat = 	40.513999, lon = 72.816098)
+        val call = weatherApi.fetchWeatherUsingQuery(lat = 40.513999, lon = 72.816098)
 
         call.enqueue(object : Callback<ForeCast> {
 
@@ -200,25 +268,5 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    private val okHttp by lazy {
-        val interceptor = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
-
-        OkHttpClient.Builder().addInterceptor(interceptor).build()
-    }
-
-    private val retrofit by lazy {
-        Retrofit.Builder()
-            //.baseUrl("https://api.openweathermap.org/data/2.5/")
-            .baseUrl("https://jsonplaceholder.typicode.com/")
-            .addConverterFactory(GsonConverterFactory.create()).client(okHttp)
-            .build()
-    }
-
-    private val weatherApi by lazy {
-        retrofit.create(WeatherApi::class.java)
-    }
-
-    private  val postsApi by lazy {
-        retrofit.create(PostsApi::class.java)
-    }
 }
+
